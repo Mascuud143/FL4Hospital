94979fb87b5f9ec8a506457b34d282d5950acad6
Tawitem <144820415+Tawitem@users.noreply.github.com>
2026-02-03 13:37:54 +0100

    delete

--- models/Room.py
+++
@@ -1,6 +0,0 @@
-class Room:
-    def __init__(self, room_id, room_number, total_sensors):
-        self.room_id = room_id
-        self.room_number = room_number
-        self.total_sensors = total_sensors
-    
