ee784c3ef8b3d895b95009b07c6af46f6c491115
Tawitem <144820415+Tawitem@users.noreply.github.com>
2026-02-03 13:37:54 +0100

    delete

--- models/Preference.py
+++
@@ -1,8 +0,0 @@
-class Preference:
-    def __init__(self, preference_id, patient_id, humidity_level, temperature_level, light_intensity, simulation_id):
-        self.preference_id = preference_id  
-        self.light_intensity = light_intensity
-        self.humidity_level = humidity_level
-        self.temperature_level = temperature_level
-        self.patient_id = patient_id
-        self.simulation_id = simulation_id
